5ed25d235ca8815c346927f35b58e4b508e368c6
nadyladwigayantry-alt <nadyladwigayantry@gmail.com>
2026-02-23 20:04:25 +0700

    Add files via upload

--- Latihan3.1.php
+++ Latihan3.1.php
@@ -1,52 +1,55 @@
 <?php
 class Kendaraan
 {
-    public $JumlahRoda=4;
+    public $JumlahRoda = 4;
     public $Warna;
-    public $BahanBakar="premium";
-    public $Harga=100000000; //
+    public $BahanBakar = "Premium";
+    public $Harga = 100000000;
     public $Merek;
-    public $tahunPembuatan=2004;
-    public function  statusHarga(): string
+    public $tahunPembuatan = 2004;
+
+    public function statusHarga(): string
     {
-        if ($this->harga>50000000)
-        {
-            $status = "Harga Kendaraan Mahal";
+        if ($this->Harga > 50000000) {
+            return "Harga Kendaraan Mahal";
+        } else {
+            return "Harga Kendaraan Murah";
         }
-        else
-        {
-            $status = "Harga Kendaraan Murah";
-        }
-    return $status;
     }
-    function statusSubsidi(): string
+
+    public function statusSubsidi(): string
     {
-        if ($this->tahunPembuatan <= 2005 && $this->BahanBakar=="Premium"){
-            $status = "DAPAT SUBSIDI";
+        if ($this->tahunPembuatan <= 2005 && $this->BahanBakar == "Premium") {
+            return "DAPAT SUBSIDI";
+        } else {
+            return "TIDAK DAPAT SUBSIDI";
         }
-        else{
-            $status = "TIDAK DAPAT SUBSIDI";
-        }
-        return $status;
     }
-       
+
+    public function hargaSecondKendaraan(): int
+    {
+        return $this->Harga - 20000000;
+    }
 }
-$ObjekKendaraan = new Kendaraan ();
+
+// Object 1
+$ObjekKendaraan = new Kendaraan();
 echo "JumlahRoda : ".$ObjekKendaraan->JumlahRoda."<br/>";
-echo "statusHarga : ". $ObjekKendaraan->statusHarga();
-echo "statusSubsidi : ". $ObjekKendaraan->statusSubsidi();
+echo "statusHarga : ".$ObjekKendaraan->statusHarga()."<br/>";
+echo "statusSubsidi : ".$ObjekKendaraan->statusSubsidi()."<br/><br/>";
 
-    $ObjekKendaraan1 = new Kendaraan;
-    $ObjekKendaraan1 ->Harga=1000000;
-    $ObjekKendaraan1 ->tahunPembuatan = 1999;
+// Object 2
+$ObjekKendaraan1 = new Kendaraan();
+$ObjekKendaraan1->Harga = 1000000;
+$ObjekKendaraan1->tahunPembuatan = 1999;
 
-    echo "statusHarga : ".$ObjekKendaraan ->statusHarga();
+echo "statusHarga : ".$ObjekKendaraan1->statusHarga()."<br/><br/>";
 
-    $ObjekKendaraan2 = new Kendaraan;
-    $ObjekKendaraan2 ->BahanBakar="pertamax";
-    $ObjekKendaraan2 ->tahunPembuatan = 1999;
+// Object 3
+$ObjekKendaraan2 = new Kendaraan();
+$ObjekKendaraan2->BahanBakar = "Premium";
+$ObjekKendaraan2->tahunPembuatan = 1999;
 
-    echo "<br/>";
-    echo "status BBM:".$ObjekKendaraan2 ->dapatSubsidi();
-    echo "<br/>";
-    echo "HargaBekas:".$ObjekKendaraan2 ->hargaSecondKendaraan();
+echo "status BBM : ".$ObjekKendaraan2->statusSubsidi()."<br/>";
+echo "Harga Bekas : ".$ObjekKendaraan2->hargaSecondKendaraan();
+?>
