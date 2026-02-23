ec2606bd94e5fe3f0b79275ed64fc10e85cf8c95
nadyladwigayantry-alt <nadyladwigayantry@gmail.com>
2026-02-23 20:22:27 +0700

    Update form.php

--- form_pegadaian.php
+++ form_pegadaian.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Form Pegadaian</title>
-</head>
-<body>
-<h2>Form Input Data Pegadaian</h2>
-<form action="proses_pegadaian.php" method="POST">
-    Pinjaman Pokok: <input type="number" name="pinjaman" required><br><br>
-    Bunga (%) : <input type="number" name="bunga" required><br><br>
-    Lama Angsuran (Bulan): <input type="number" name="lama" required><br><br>
-    Keterlambatan Bayar (Hari): <input type="number" name="keterlambatan" required><br><br>
-    Denda Per Hari (%) : <input type="number" name="denda" step="0.01" required><br><br>
-    <input type="submit" value="Hitung">
-</form>
-</body>
-</html>
+
