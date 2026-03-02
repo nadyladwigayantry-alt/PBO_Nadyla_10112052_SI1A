f234c5f3a95f021c7b60038b2b0ce83ccacbc2c4
nadyladwigayantry-alt <nadyladwigayantry@gmail.com>
2026-03-03 06:22:03 +0700

    Add files via upload

---
+++ Form_produk5.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Belanja Warung A</title>
+</head>
+<body>
+
+<h2>Form Belanja Warung A</h2>
+
+<form method="POST" action="Proses_produk5.php">
+
+Nama Pembeli:<br>
+<input type="text" name="nama"><br><br>
+
+Nama Barang:<br>
+<input type="text" name="barang"><br><br>
+
+Harga:<br>
+<input type="text" name="harga"><br><br>
+
+Jumlah:<br>
+<input type="text" name="jumlah"><br><br>
+
+<button type="submit">Proses Belanja</button>
+
+</form>
+</body>
+</html>
